a957eacb3eacd256d9018808587192d3bc28d41e
Chase Abram <abram@uchicago.edu>
2021-04-05 08:39:00 -0500

    learning

--- FTPL_HW.jl
+++ FTPL_HW.jl
@@ -1,2 +1,21 @@
-println("banana")
+using Pkg
+
+using CSV
+using DataFrames
+using ForwardDiff
+using LinearAlgebra
+using Optim
+
+using Random, Distributions
+using Statistics
+using StatsPlots
+
+using Plots
+
+##
+
+x = LinRange(1,10,100)
+y = x.^2
+println(y)
+
 
