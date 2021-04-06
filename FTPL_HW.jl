cc7e6a67b1691763fae3e22b7a368eff9cf6c1b8
Chase Abram <abram@uchicago.edu>
2021-04-06 09:54:40 -0500

    f

--- FTPL_HW.jl
+++ FTPL_HW.jl
@@ -11,34 +11,57 @@ using Statistics
 using StatsPlots
 
 using Plots
+# Pkg.add("Parameters")
+using Parameters
 
 ##
 
-# x = LinRange(1,10,100)
-# y = x.^2
-# println("1")
-# println(y.^2)
+# Parameters (need to check against a paper)
+rho = 0.05
+gamma = 1
+epsilon = 1.1
+theta = 5
+varphi = 1
+nu = 0.5
 
 
-# plot(x,y.^6)
+# Policy
+@with_kw struct Policyparam
+    @deftype Float64
+    # MP
+    i_star = 0.05 # Should probably make match natural real
+    phi_pi = 1.1
+    phi_b = 0.0
+    phi_x = 0.0
+    # FP
+    tau_star = 0
+    psi_pi = 0.0
+    psi_b = 0.0
+    psi_x = 0.0
+end
 
-# cache = Dict()
-# cache[1] = 1
-# cache[2] = 2
-# function fib(n)
-#     # println("fib on ", n, " called")
-#     if n in keys(cache)
-#         return cache[n]
-#     end
-#     cache[n] = fib(n-1) + fib(n-2)
-#     return cache[n]
-# end
 
-# # println(fib(1))
-# # println(fib(2))
-# # println(fib(3))
-# # println(fib(4))
-# println(fib(800))
+# Utility
+function u(c, g) 
+    if g == 1
+        return log(c)
+    else
+        return (c^(1-g) - 1)/(1-g)
+    end
+end
+
+# Marginal Utility
+function up(c,g)
+    if g == 1
+        return 1/c
+    else
+        return c^(-g)
+    end
+end
+
+function dpi(p)
+
+
 
 
 
