479b5ad096aaadd59941aac622962a304d35cea6
Chase Abram <abram@uchicago.edu>
2021-04-08 15:02:55 -0500

    Seed of EGP written

--- AnnualReviewsHA/EGP.jl
+++ AnnualReviewsHA/EGP.jl
@@ -1,5 +1,4 @@
-
-
+using Params.jl
 
 # Chase Abram
 # For solving HA discrete-time models via endogenous grid point
@@ -8,20 +7,39 @@
 
 function solve_EGP(p, grids, income)
 
-    @unpack_Params
+    @unpack_Params p
     # unpack other structs also?
 
     # Guess cons
-    cons = r.*a_grid + y_grid
+    con_old = r.*aa_grid + yyF_grid + yyP_grid + yyT_grid
+    p.con = con_old
 
     egp_iter = 0
     egp_diff = Inf
 
     while egp_iter < egp_maxiter && egp_diff > egp_tol
 
-        # Expected MUC
-        
+        # MU
+        up(p)
 
+        upinv(p)
+
+        emuc(p)
+
+        muc(p)
+
+        update_con_euler(p)
+
+        update_a_euler(p)
+
+        interp_a_tom(p)
+
+        update_con(p)
+
+        egp_diff = maximum(abs.(p.con - con_old))
+
+        con_old = p.con
+        egp_iter += 1
     end
 
 
