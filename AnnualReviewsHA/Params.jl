ed0d7cc58e5366595b5304321b18dcfdada25c91
Chase Abram <abram@uchicago.edu>
2021-04-09 08:17:07 -0500

    j

--- AnnualReviewsHA/Params.jl
+++ AnnualReviewsHA/Params.jl
@@ -5,6 +5,8 @@ using Pkg
 # Pkg.add("Interpolations")
 using Interpolations
 
+##
+
 # Structure to hold all parameters and the setup functions of the problem
 
 @with_kw mutable struct Params
@@ -38,15 +40,20 @@ using Interpolations
     zgrid = LinRange(0,nz,nz)
 
     # Build augmented grids (Fix this!)
-    aagrid = repeat(reshape(agrid, (na,1,1,1,1)),
-    1, nyF, nyP, nyT, nz)
+    aagrid = repeat(reshape(agrid, (na,1,1,1,1)), 1, nyF, nyP, nyT, nz)
 
-    # yyFgrid = repeat(reshape(yFgrid, (1,nyF,1,1,1)), na, 1, nyP, nyT, nz)
+    yyFgrid = repeat(reshape(yFgrid, (1,nyF,1,1,1)), na, 1, nyP, nyT, nz)
+
+    yyPgrid = repeat(reshape(yPgrid, (1,1,nyP,1,1)), na, nyF, 1, nyT, nz)
+
+    yyTgrid = repeat(reshape(yTgrid, (1,1,1,nyT,1)), na, nyF, nyP, 1, nz)
+
+    zzgrid = repeat(reshape(zgrid, (1,1,1,1,nz)), na, nyF, nyP, nyT, 1)
 
 
 
     # income transitions (needs to be set up)
-    income_trans = [ones(na,nyF,nyP,nyT,nz) for a in 1:na, yF in 1:nyF, yP in 1:nyP, yT in 1:nyT, z in 1:nz]
+    income_trans = [ones(na,nyF,nyP,nyT,nz)/(na*nyF*nyP*nyT*nz) for a in 1:na, yF in 1:nyF, yP in 1:nyP, yT in 1:nyT, z in 1:nz]
 
     # Consumption as function of assets tomorrow and income today
     con_euler = zeros(na,nyF,nyP,nyT,nz)
@@ -101,7 +108,7 @@ function emuc(p)
             for yP in 1:p.nyP
                 for yT in 1:nyT
                     for z in 1:p.nz
-                        p.emuc[a,yF,yP,yT,z] = sum(up(p) .* p.income_trans[a,yF,yP,yT,z])
+                        p.emuc[a,yF,yP,yT,z] = sum(p.up .* p.income_trans[a,yF,yP,yT,z])
                     end
                 end
             end
@@ -140,11 +147,19 @@ function update_con(p)
 end
 
 
-p0 = Params()
+
+
+
+
+
+
+##
+
+# p0 = Params()
 # println(p0)
 
-@unpack na,nyF,nyP,nyT,nz = p0
-c0 = exp.(randn(na,nyF,nyP,nyT,nz))
+# @unpack na,nyF,nyP,nyT,nz = p0
+# c0 = exp.(randn(na,nyF,nyP,nyT,nz))
 
 # println("1. maximum(p0.up): ", maximum(p0.up), ", size(p0.up): ", size(p0.up))
 # up(p0, c0[1,1,1,1,1])
@@ -155,6 +170,6 @@ c0 = exp.(randn(na,nyF,nyP,nyT,nz))
 # p0.emuc
 # p0.up = 5
 # println("2. maximum(p0.up): ", maximum(p0.up))
-println("1. r: ", p0.r, ", R: ", p0.R)
-p0.r = 0.5
-println("2. r: ", p0.r, ", R: ", p0.R)
+# println("1. r: ", p0.r, ", R: ", p0.R)
+# p0.r = 0.5
+# println("2. r: ", p0.r, ", R: ", p0.R)
